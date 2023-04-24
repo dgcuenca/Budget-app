0228e4fb5f0a360fc137a6faf2dc288d1b1a01f0
diego <dgcuenca@gmail.com>
2023-04-24 11:06:07 -0500

    Set up relatinal database in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :entitys, dependent: :destroy
+  has_many :groups, dependent: :destroy
+  validates :name, presence: true
 end
