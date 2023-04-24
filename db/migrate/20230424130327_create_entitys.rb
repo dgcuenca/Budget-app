7ed6672b584905c00443df15cb97ad40cbad47a2
diego <dgcuenca@gmail.com>
2023-04-24 10:01:48 -0500

    Create database models

--- db/migrate/20230424130327_create_entitys.rb
+++ db/migrate/20230424130327_create_entitys.rb
@@ -2,9 +2,8 @@ class CreateEntitys < ActiveRecord::Migration[7.0]
   def change
     create_table :entitys do |t|
       t.string :name
-      t.decimal :amount, :decimal, precision: 6, scale: 2
+      t.decimal :amount, precision: 6, scale: 2
       t.references :user, null: false, foreign_key: true
-      t.references :group, null: false, foreign_key: true
 
       t.timestamps
     end
