f0e262d2c22f12e937efc5c6ef665a54becd3cd3
diego <dgcuenca@gmail.com>
2023-04-25 07:01:41 -0500

    Add validations to entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,6 @@
 class Entity < ApplicationRecord    
     belongs_to :user
     has_and_belongs_to_many :groups
+    validates :amount, presence: true, format: { with: /\A\d+(\.\d{1,2})?\z/, message: "must be a decimal value with up to 2 decimal places" }, numericality: { greater_than_or_equal_to: 0 }
+    validates :name, presence: true, uniqueness: true
 end
