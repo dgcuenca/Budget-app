62aab1e67938c135df072cf9d7d6103f98d5de6b
diego <dgcuenca@gmail.com>
2023-04-26 12:14:15 -0500

    Set up active storage and migrate it

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :entitys
+  has_one_attached :icon
   validates :name, presence: true, uniqueness: true
   validates :icon, presence: true
   validate :icon_format
-
+  
   private
 
   def icon_format
