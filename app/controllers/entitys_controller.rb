b07fa59f5e01e93672158c478867c72b6778a83e
diego <dgcuenca@gmail.com>
2023-04-26 08:15:53 -0500

    Enable creation of new category

--- app/controllers/entitys_controller.rb
+++ app/controllers/entitys_controller.rb
@@ -1,5 +1,31 @@
 class EntitysController < ApplicationController
+
     def index
       @entities = current_user.entitys
-    end  
+    end
+
+    def new
+      @current_user = current_user
+      @entity = Entity.new
+    end
+
+    def create
+      @current_user = current_user
+      @entity = @current_user.entitys.create(entity_params)
+      if @entity.save
+        redirect_to entitys_path
+      else
+        render :new
+      end
+    end
+
+    def show
+      
+    end
+
+    private
+
+    def entity_params
+      params.require(:entity).permit(:name)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_132325) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_26_112015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "entities", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount", precision: 6, scale: 2
+    t.decimal "amount", precision: 6, scale: 2, default: "0.0"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
