8868aca8eb66e343bb032b508e12ca52e42522a8
diego <dgcuenca@gmail.com>
2023-04-29 12:36:55 -0500

    Add tests for entity new view

---
+++ spec/views/entity_new_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require_relative 'actions_helper_entity'
+
+RSpec.describe Group, type: :system do
+  include ActionsHelperEntity
+  it 'Can see the Group Heding' do
+    sign_up
+    add_group
+    add_transaction
+    visit groups_path
+    click_link 'House'
+    click_link 'ADD AN EXPENSE'
+    expect(page).to have_content('ADD EXPENSE')
+  end
+
+end
